a1ab24f9ca1102740ec797c2d3ea12f94e54b6b2
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-05-05 15:09:41 +0000

    Deploying to gh-pages from @ Serhieie/ReactOctopus@0e7b2fbd01f5a2cae07fa1b3c72db4d1fdbff769 🚀

--- assets/ScreenPage-0d5c8cff.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-3a742781.js";const t=()=>e.jsx("div",{children:"ScreenPage"});export{t as default};
---
+++ assets/ScreenPage-7c2e4315.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-d64ad359.js";const t=()=>e.jsx("div",{children:"ScreenPage"});export{t as default};
