5bdac15ffc5d9ca40e718031a513781d10dad959
OlesiaZaozirna <137696365+OlesiaZaozirna@users.noreply.github.com>
2024-05-06 23:40:09 +0300

    fixed styles

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -4,7 +4,7 @@ import Welcome from '../../components/Welcome/Welcome';
 
 const WelcomePage = () => {
   return (
-    <div>
+    <div style={{background: 'linear-gradient(180.00deg, rgba(196, 196, 196, 0) 25%, rgb(190, 219, 176) 92.19%)'}}>
       <Welcome />
     </div>
   );
