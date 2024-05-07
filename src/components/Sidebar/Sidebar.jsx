f28788c8c14ccbedcd35a582033b9c3f96ab734a
Havrylyuk Pavlo <havrylyukpavlo95@gmail.com>
2024-05-07 17:32:10 +0300

    push in to main

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,25 +10,25 @@ const Sidebar = () => {
   const theme = 'dark';
   const isSidebarOpen = false;
   const { isMobile, isTablet, isDesktop } = useMedia();
-  // const endOfURL = location.pathname === '/home';
+  const endOfURL = location.pathname === '/home';
 
   return (
     <>
-      {/* {endOfURL && ( */}
-      <div
-        className={clsx(styles.sidebar_container, {
-          [styles.sidebar_containerDark]: theme === 'dark',
-          [styles.sidebar_containerLight]: theme === 'light',
-          [styles.sidebar_containerViolet]: theme === 'violet',
-          [styles.sidebar_hidden]: isMobile || isTablet,
-          [styles.sidebar_open]: isDesktop || isSidebarOpen,
-        })}
-      >
-        <Logo theme={theme} />
-        <BoardList theme={theme} />
-        <NeedHelp />
-      </div>
-      {/* )} */}
+      {endOfURL && (
+        <div
+          className={clsx(styles.sidebar_container, {
+            [styles.sidebar_containerDark]: theme === 'dark',
+            [styles.sidebar_containerLight]: theme === 'light',
+            [styles.sidebar_containerViolet]: theme === 'violet',
+            [styles.sidebar_hidden]: isMobile || isTablet,
+            [styles.sidebar_open]: isDesktop || isSidebarOpen,
+          })}
+        >
+          <Logo theme={theme} />
+          <BoardList theme={theme} />
+          <NeedHelp />
+        </div>
+      )}
     </>
   );
 };
