dfce197eeabf2a21663fffe86181a0ff50998ee1
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-05-08 08:35:43 +0300

    fix: useEffect

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -2,17 +2,22 @@ import { useEffect } from 'react';
 import Welcome from '../../components/Welcome/Welcome';
 import { ping } from '../../redux/api/api';
 
-const wakeUp = async () => {
-  await ping();
-};
-
-useEffect(() => {
-  wakeUp();
-}, []);
-
 const WelcomePage = () => {
+  const wakeUp = async () => {
+    await ping();
+  };
+
+  useEffect(() => {
+    wakeUp();
+  }, []);
+
   return (
-    <div style={{background: 'linear-gradient(180.00deg, rgba(196, 196, 196, 0) 25%, rgb(190, 219, 176) 92.19%)'}}>
+    <div
+      style={{
+        background:
+          'linear-gradient(180.00deg, rgba(196, 196, 196, 0) 25%, rgb(190, 219, 176) 92.19%)',
+      }}
+    >
       <Welcome />
     </div>
   );
