4b79f053e53e846d2587acc26982da343dc9b133
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-05-08 08:33:20 +0300

    test

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-
 import Welcome from '../../components/Welcome/Welcome';
 
 const WelcomePage = () => {
   return (
-    <div style={{background: 'linear-gradient(180.00deg, rgba(196, 196, 196, 0) 25%, rgb(190, 219, 176) 92.19%)'}}>
+    <div
+      style={{
+        background:
+          'linear-gradient(180.00deg, rgba(196, 196, 196, 0) 25%, rgb(190, 219, 176) 92.19%)',
+      }}
+    >
       <Welcome />
     </div>
   );
