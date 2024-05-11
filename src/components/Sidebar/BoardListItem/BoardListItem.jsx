4613c711138b106a8afc9d89b854e04f02a2508e
Havrylyuk Pavlo <havrylyukpavlo95@gmail.com>
2024-05-11 19:24:53 +0300

    save

--- src/components/Sidebar/BoardListItem/BoardListItem.jsx
+++ src/components/Sidebar/BoardListItem/BoardListItem.jsx
@@ -7,11 +7,18 @@ import EditBoard from '../../popUps/Board/EditBoard';
 import { DeleteModal } from '../../MainScreen/DeleteModal/DeleteModal';
 import { useDispatch } from 'react-redux';
 import { fetchBoardById } from '../../../redux/tasks/boards/boardsOperations';
+// import { useSelector } from 'react-redux';
+// import { selectBoardsState } from '../../../redux/tasks/tasksSelectors';
+import { NavLink } from 'react-router-dom';
 
-const BoardListItem = ({ theme, board }) => {
+const BoardListItem = ({ theme, board, activeItem }) => {
   const [isEditBoardModalOpen, setIsEditBoardModalOpen] = useState(false);
   const [isDeleteBoardModalOpen, setIsDeleteBoardModalOpen] = useState(false);
   const dispatch = useDispatch();
+
+  // const { active } = useSelector(selectBoardsState);
+
+  console.log(activeItem);
 
   const toggleEditBoardModalOpen = () => {
     setIsEditBoardModalOpen((state) => !state);
@@ -22,74 +29,82 @@ const BoardListItem = ({ theme, board }) => {
   };
 
   const handleChangeBoard = () => {
-    dispatch(fetchBoardById(board._id));
+    // dispatch(fetchBoardById(board._id));
   };
 
   return (
-    <li
-      onClick={handleChangeBoard}
-      className={clsx(styles.sidebar_board_item, {
-        [styles.sidebar_board_itemDark]: theme === 'dark',
-        [styles.sidebar_board_itemLight]: theme === 'light',
-        [styles.sidebar_board_itemViolet]: theme === 'violet',
-      })}
-    >
-      <div className={styles.right_part}>
-        {' '}
-        <div className={styles.sidebar_board_active}></div>
-        <div className={styles.sidebar_boart_cont}>
-          <button
-            onClick={toggleDeleteBoardModalOpen}
-            type="button"
-            className={styles.sidebar_board_remove_btn}
-          >
-            <svg
-              className={styles.sidebar_board_remove_ico}
-              width="16"
-              height="16"
+    <NavLink to={`/${board.title}`}>
+      <li
+        onClick={handleChangeBoard}
+        className={clsx(styles.sidebar_board_item, {
+          [styles.sidebar_board_itemDark]: theme === 'dark',
+          [styles.sidebar_board_itemLight]: theme === 'light',
+          [styles.sidebar_board_itemViolet]: theme === 'violet',
+          [styles.board_activeItemDark]:
+            activeItem === 'active' && theme === 'dark',
+          [styles.board_activeItemLight]:
+            activeItem === 'active' && theme === 'light',
+          [styles.board_activeItemViolet]:
+            activeItem === 'active' && theme === 'violet',
+        })}
+      >
+        <div className={styles.right_part}>
+          {' '}
+          <div className={styles.sidebar_board_active}></div>
+          <div className={styles.sidebar_board_cont}>
+            <button
+              onClick={toggleDeleteBoardModalOpen}
+              type="button"
+              className={styles.sidebar_board_remove_btn}
             >
-              <use xlinkHref={`${LogoSprite}#icon-trash`}></use>
-            </svg>
-          </button>
-          <button
-            onClick={toggleEditBoardModalOpen}
-            type="button"
-            className={styles.sidebar_board_edit_btn}
-          >
-            <svg
-              className={styles.sidebar_board_edit_ico}
-              width="16"
-              height="16"
+              <svg
+                className={styles.sidebar_board_remove_ico}
+                width="16"
+                height="16"
+              >
+                <use xlinkHref={`${LogoSprite}#icon-trash`}></use>
+              </svg>
+            </button>
+            <button
+              onClick={toggleEditBoardModalOpen}
+              type="button"
+              className={styles.sidebar_board_edit_btn}
             >
-              <use xlinkHref={`${LogoSprite}#icon-pencil`}></use>
-            </svg>
-          </button>
+              <svg
+                className={styles.sidebar_board_edit_ico}
+                width="16"
+                height="16"
+              >
+                <use xlinkHref={`${LogoSprite}#icon-pencil`}></use>
+              </svg>
+            </button>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.sidebar_boart_cont}>
-        <p className={styles.sidebar_board_title}>{board.title}</p>
-        <svg className={styles.sidebar_board_item_ico} width="18" height="18">
-          <use xlinkHref={`${LogoSprite}#icon-${board.iconId}`}></use>
-        </svg>
-      </div>
-      <ModalPortal>
-        <EditBoard
-          name="Edit board"
-          open={isEditBoardModalOpen}
-          item={board}
-          func={toggleEditBoardModalOpen}
-        />
-      </ModalPortal>
-      <ModalPortal>
-        <DeleteModal
-          func={toggleDeleteBoardModalOpen}
-          itemType="board"
-          open={isDeleteBoardModalOpen}
-          item={board}
-        />
-      </ModalPortal>
-    </li>
+        <div className={styles.sidebar_board_cont}>
+          <p className={styles.sidebar_board_title}>{board.title}</p>
+          <svg className={styles.sidebar_board_item_ico} width="18" height="18">
+            <use xlinkHref={`${LogoSprite}#icon-${board.iconId}`}></use>
+          </svg>
+        </div>
+        <ModalPortal>
+          <EditBoard
+            name="Edit board"
+            open={isEditBoardModalOpen}
+            item={board}
+            func={toggleEditBoardModalOpen}
+          />
+        </ModalPortal>
+        <ModalPortal>
+          <DeleteModal
+            func={toggleDeleteBoardModalOpen}
+            itemType="board"
+            open={isDeleteBoardModalOpen}
+            item={board}
+          />
+        </ModalPortal>
+      </li>
+    </NavLink>
   );
 };
 
